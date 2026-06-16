14591428e71a22693dafe2d1ef27c99b733a288a
Hoon Ma <mahoon2@snu.ac.kr>
2026-06-16 19:01:20 +0900

    feat: remove oh-my-zsh

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,31 @@
+# Personal aliases. OMZ plugin aliases are loaded from their original plugin files.
+
+alias ls="eza -l --icons=\"auto\" --group-directories-first"
+alias vi="nvim"
+
+# Python
+alias python="python3"
+alias pip="pip3"
+alias pi="pip3 install"
+alias pui="pip3 uninstall"
+
+# Python virtualenv
+alias va='source .venv/bin/activate'
+alias vda='deactivate'
+
+# Conda
+alias ca="conda activate"
+alias cda="conda deactivate"
+alias ci="conda install"
+alias cui="conda uninstall"
+alias cls="conda list"
+alias cels="conda env list"
+
+# CLI tools
+alias cc="claude"
+alias ccu="ccusage"
+alias gem="gemini"
+alias co="codex"
+
+# Git
+alias gfp="git fetch --prune"
